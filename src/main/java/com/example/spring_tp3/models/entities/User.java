5e8d8e834213883a164d74cb82097b79a56978e3
ManonKerrels <manon.kerrels@gmail.com>
2022-04-20 13:47:03 +0200

    User
    +table

---
+++ src/main/java/com/example/spring_tp3/models/entities/User.java
@@ -0,0 +1,56 @@
+package com.example.spring_tp3.models.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private boolean isNotLocked = true;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    List<String> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles().stream()
+                .map( SimpleGrantedAuthority::new )
+                .toList();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return isNotLocked; }
+
+    @Override
+    public boolean isAccountNonLocked() { return isNotLocked; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return isNotLocked; }
+
+    @Override
+    public boolean isEnabled() { return isNotLocked; }
+}
---
+++ src/main/java/com/example/spring_tp3/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.spring_tp3.repository;
+
+import com.example.spring_tp3.models.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+}
